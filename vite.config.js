ebb5cc6ad7bee4039df90e815d658173d3455182
jagan <jagan@gmail.com>
2025-11-21 13:30:47 +0530

    Fix: correct paths + rebuild

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/JaganPortfolio/",  // <-- IMPORTANT
+  base: "/JagannathPortfolio/",  // <-- IMPORTANT
 });
